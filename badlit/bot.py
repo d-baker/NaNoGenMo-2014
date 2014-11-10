29ed369d9f912f5d994017921f66f38bb206c1b8
dbaker <d-_-baker@hotmail.com>
2014-11-10 21:42:51 +1100

    new sentence structurs from Dorian Gray

--- badlit/bot.py
+++ badlit/bot.py
@@ -3,41 +3,100 @@ from __future__ import unicode_literals
 import random
 from pattern.en import referenced, pluralize
 
-def gen():
-    adjs = list(open("resources/adjectives.txt").read().split())
-    adverbs = list(open("resources/adverbs.txt").read().split())
-    adjeys = list(open("resources/adjectively.txt").read().split())
-    ns = list(open("resources/nouns.txt").read().split())
-    verbed = list(open("resources/verbed.txt").read().split())
-    verbing = list(open("resources/verbing.txt").readlines())
-    quants = list(open("resources/quantities.txt").read().split())
-    places = list(open("resources/cities.txt").read().split())
-    qualities = list(open("resources/qualities.txt").read().split())
-    
-    opening = "It was {adjective} and {adjectively} {noun}.".format(adjective=referenced(random.choice(adjs)), adjectively=random.choice(adjeys), noun=random.choice(ns))
+adjs = list(open("resources/adjectives.txt").readlines())
+adverbs = list(open("resources/adverbs.txt").readlines())
+adjeys = list(open("resources/adjectively.txt").readlines())
+ns = list(open("resources/nouns.txt").readlines())
+verbed = list(open("resources/verbed.txt").readlines())
+verbing = list(open("resources/verbing.txt").readlines())
+quants = list(open("resources/quantities.txt").readlines())
+places = list(open("resources/cities.txt").readlines())
+qualities = list(open("resources/qualities.txt").readlines())
 
-    paragraph = "The {n1} {verbed1} in {quant1} — except at occasional intervals, when it was checked by {adj1} {quant2} of {n2} which {verbed2} (for it is in {place} that our scene lies), {verbing1} the {n3}, and {adverbly} {verbing2} the {adjective} {n4} of the {nouns} that {verbed3} against the {nounness}".format(
-        n1=random.choice(ns),
-        verbed1=random.choice(verbed),
-        quant1=pluralize(random.choice(ns)),
-        #quant1=random.choice(quants),
-        adj1=random.choice(adjs),
-        quant2=random.choice(quants),
-        n2=random.choice(ns),
-        verbed2=random.choice(verbed),
-        place=random.choice(places),
+def opening():
+    p = "It was {adjective} and {adjectively} {noun}.".format(
+        adjective=referenced(random.choice(adjs)).strip(), 
+        adjectively=random.choice(adjeys).strip(), 
+        noun=random.choice(ns).strip()
+        )
+    return p
+
+def p1():
+    p = "The {n1} {verbed1} {quant1} — except at occasional intervals, when it was checked by {adj1} {quant2} of {n2} which {verbed2} {nouns1} (for it is in {place} that our scene lies), {verbing1} the {n3}, and {adverbly} {verbing2} the {adjective} {n4} of the {nouns2} that {verbed3} the {nounness}.".format(
+        n1=random.choice(ns).strip(),
+        verbed1=random.choice(verbed).strip(),
+        #quant1=pluralize(random.choice(ns).strip()),
+        quant1=random.choice(quants).strip(),
+        adj1=random.choice(adjs).strip(),
+        quant2=random.choice(quants).strip(),
+        n2=random.choice(ns).strip(),
+        verbed2=random.choice(verbed).strip(),
+        nouns1=pluralize(random.choice(ns).strip()),
+        place=random.choice(places).strip(),
         verbing1=random.choice(verbing).strip(),
-        n3=random.choice(ns),
-        adverbly=random.choice(adverbs),
+        n3=random.choice(ns).strip(),
+        adverbly=random.choice(adverbs).strip(),
         verbing2=random.choice(verbing).strip(),
-        adjective=random.choice(adjs),
-        n4=random.choice(ns),
-        nouns=pluralize(random.choice(ns)),
-        verbed3=random.choice(verbed),
-        nounness=random.choice(qualities)
+        adjective=random.choice(adjs).strip(),
+        n4=random.choice(ns).strip(),
+        nouns2=pluralize(random.choice(ns).strip()),
+        verbed3=random.choice(verbed).strip(),
+        nounness=random.choice(qualities).strip()
     )
 
-    return opening + "\n" + paragraph
+    return p
+
+def p2():
+    n1 = random.choice(ns).strip()
+
+    p = "The {noun1} is the {noun2} of {adjective1} {things}.  To reveal {nouns1} and conceal the {noun2} is the {noun1s} aim. Those who find {adjective2} meanings in {adjective1} {things} are {adjective3} without being {adjective4}.".format(
+        noun1 = n1,
+        noun2 = random.choice(ns).strip(),
+        adjective1 = random.choice(adjs).strip(),
+        things = pluralize(random.choice(ns).strip()),
+        nouns1 = pluralize(random.choice(ns).strip()),
+        noun1s = n1 + "'s",
+        adjective2 = random.choice(adjs).strip(),
+        adjective3 = random.choice(adjs).strip(),
+        adjective4 = random.choice(adjs).strip()
+    )
+
+    return p
+
+def p3():
+    p = "The {place} was filled with the {adjective1} {noun1} of {nouns1}, and when the {adjective2} {adjective3} {noun2} stirred admist the {nouns2} of the {noun3}, there came through the {adjective4} {noun4} the {adjective5} {noun5} of the {noun6}, or the more {adjective6} {noun7} of the {adjective7} {noun8}.".format(
+        place = random.choice(ns).strip(),
+        adjective1 = random.choice(adjs).strip(),
+        noun1 = random.choice(ns).strip(),
+        nouns1 = pluralize(random.choice(ns).strip()),
+        adjective2 = random.choice(adjs).strip(),
+        adjective3 = random.choice(adjs).strip(),
+        noun2 = random.choice(ns).strip(),
+        nouns2 = pluralize(random.choice(ns).strip()),
+        noun3 = random.choice(ns).strip(),
+        adjective4 = random.choice(adjs).strip(),
+        noun4 = random.choice(ns).strip(),
+        adjective5 = random.choice(adjs).strip(),
+        noun5 = random.choice(ns).strip(),
+        noun6 = random.choice(ns).strip(),
+        adjective6 = random.choice(adjs).strip(),
+        noun7 = random.choice(ns).strip(),
+        adjective7 = random.choice(adjs).strip(),
+        noun8 = random.choice(ns).strip()
+    )
+
+    return p
+
+def gen():
+    text = ""
+
+    for i in range(0, random.randint(1, 5)):
+        text += "\n" + random.choice([opening(), p1(), p2(), p3()]) + "\n"
+
+    return text
+
+
 
 if __name__ == "__main__":
-    print gen()
+    for i in range (0, 5):
+        print gen()
